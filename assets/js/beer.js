f9bc37b12725288907cf4e16dba0e5aec4cda2b1
megharpx <meghaneharper@gmail.com>
2022-07-18 14:44:01 -0400

    updated beer.js

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -1,5 +1,5 @@
 function searchBeer(lat,long) {
-   //  var beerCode = document.querySelector("#brewery_name").value;
+   // var beerCode = document.querySelector("#brewery_name").value;
   console.log(lat,long)
     var getBeerInfo =
       "https://api.openbrewerydb.org/breweries?by_dist="+lat+","+long+"&per_page=3";
@@ -20,17 +20,42 @@ function searchBeer(lat,long) {
         }
       });
     console.log(options);
-  }
+
+  getBeerInfo.innerHTML = "";
+      var breweryName = document.createElement("h2");
+      breweryName.textContent = response.name;
+      getBeerInfo.prepend(breweryName);
+
+      var address = document.createElement("p");
+      address.textContent = response.street_number + " " + response.street;
+      getBeerInfo.append(address)
+      
+      var address2 = document.createElement("p");
+      address2.textContent = response.city + ", " + response.state + " " + response.postal_code;
+      getBeerInfo.append(address2)
+
+      var phone = document.createElement("p");
+      phone.textContent = response.phone;
+      getBeerInfo.append(phone)
+      
+      var website = document.createElement("p");
+      var link = document.createElement("a");
+      link.setAttribute("href", response.website);
+      link.setAttribute("target", "_blank");
+      link.textContent = response.website;
+      website.append(link);
+      getBeerInfo.append(website);
+
+      beerCode.value = "lat, long";
+    }
+    
+   // .catch(function (error) {
+    //  console.log("error", error);
+    //});
+  //console.log(options);
+//}
+
   
 //   document.addEventListener("click", searchBeer)
 document.querySelector(".btn-search").addEventListener("click", searchBeer)
-
-// module.exports = {
- // doSomething: function() {
-    //code
-  //},
-
- // anotherOne: function() {
-    //code
- // }
-//  };
+  
