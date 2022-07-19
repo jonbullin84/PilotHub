cf43441adbc9fdc1005821f1ff25eb81d85f55f0
Brian King <bnk5532@gmail.com>
2022-07-19 17:40:49 -0400

    debug beer.js

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -1,61 +1,58 @@
-function searchBeer(lat,long) {
-   // var beerCode = document.querySelector("#brewery_name").value;
-  console.log(lat,long)
-    var getBeerInfo =
-      "https://api.openbrewerydb.org/breweries?by_dist="+lat+","+long+"&per_page=3";
-  
-    fetch(getBeerInfo, options)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        console.log(response);
-        if (response.cod !== 200) {
-          document.getElementById("#brewery_name");
-          isError = true;
-          return;
-        }
-        if (!isError) {
-          saveLocal(options);
-        }
-      });
-    console.log(options);
 
-  getBeerInfo.innerHTML = "";
+
+var beerInfo = document.querySelector(".beer-info")
+
+function searchBeer(lat, long) {
+  // var beerCode = document.querySelector("#brewery_name").value;
+  console.log(lat, long);
+  var getBeerInfo =
+    "https://api.openbrewerydb.org/breweries?by_dist=" +
+    lat +
+    "," +
+    long +
+    "&per_page=3";
+
+  fetch(getBeerInfo, options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    
+      beerInfo.innerHTML = "";
       var breweryName = document.createElement("h2");
       breweryName.textContent = response.name;
-      getBeerInfo.prepend(breweryName);
+      beerInfo.prepend(breweryName);
 
       var address = document.createElement("p");
       address.textContent = response.street_number + " " + response.street;
-      getBeerInfo.append(address)
-      
+      beerInfo.append(address);
+
       var address2 = document.createElement("p");
-      address2.textContent = response.city + ", " + response.state + " " + response.postal_code;
-      getBeerInfo.append(address2)
+      address2.textContent =
+        response.city + ", " + response.state + " " + response.postal_code;
+      beerInfo.append(address2);
 
       var phone = document.createElement("p");
       phone.textContent = response.phone;
-      getBeerInfo.append(phone)
-      
+      beerInfo.append(phone);
+
       var website = document.createElement("p");
       var link = document.createElement("a");
       link.setAttribute("href", response.website);
       link.setAttribute("target", "_blank");
       link.textContent = response.website;
       website.append(link);
-      getBeerInfo.append(website);
+      beerInfo.append(website);
 
-      beerCode.value = "lat, long";
-    }
-    
-   // .catch(function (error) {
-    //  console.log("error", error);
-    //});
-  //console.log(options);
+      // beerCode.value = "lat, long";
+    })
+    .catch(function (error) {
+      console.log("error", error);
+    });
+}
+console.log("end of script");
 //}
 
-  
 //   document.addEventListener("click", searchBeer)
-document.querySelector(".btn-search").addEventListener("click", searchBeer)
-  
+// document.querySelector(".btn-search").addEventListener("click", searchBeer)
